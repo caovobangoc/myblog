63b79cf4d993df0353a421416834ad43e70c3761
Chau <chauta2204@gmail.com>
2018-11-11 15:03:19 +0700

    Add myblog

---
+++ app/config/config.py
@@ -0,0 +1,24 @@
+import os
+from os.path import abspath, join, dirname
+from dotenv import load_dotenv
+
+basedir = abspath(dirname(__file__))
+
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
+class Config(object):
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    MAIL_SERVER = os.environ.get('MAIL_SERVER')
+    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    ADMINS = ['your-email@example.com']
+
+    POSTS_PER_PAGE = 10
+    # LANGUAGES = ['en', 'es']
---
+++ app/views/forms/__init__.py
@@ -0,0 +1,6 @@
+from .edit_profile import EditProfileForm
+from .login import LoginForm
+from .registration import RegistrationForm
+from .post import PostForm
+from .reset_password_request import ResetPasswordRequestForm
+from .reset_password import ResetPasswordForm
